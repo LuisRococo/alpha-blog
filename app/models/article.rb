0c187e0cd89828f6f345803bffa9717d2034bbd3
Luis Roberto <luisrobertopenav@gmail.com>
2022-08-08 15:59:48 -0700

    Render page article from param id

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord    
-    validates :title, presence: true, length: {minimal: 6, maximal: 100}
-    validates :description, presence: true, length: {minimal: 10, maximal: 300}
+    validates :title, presence: true, length: {minimum: 6, maximal: 100}
+    validates :description, presence: true, length: {minimum: 10, maximal: 300}
 end
