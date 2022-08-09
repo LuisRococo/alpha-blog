3ebd76a4026736b5ae552a24baf9e47bdf56eb02
Luis Roberto <luisrobertopenav@gmail.com>
2022-08-08 17:43:32 -0700

    Add error and success message on creating article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,12 +8,19 @@ class ArticlesController < ApplicationController
     end
 
     def new ()
+        # so the first time the page loads, it does not generate any error
+        @article = Article.new()
     end
 
     def create ()
         @article = Article.new(params.require(:article).permit(:title, :description))
-        @article.save
-        # redirect_to article_path(@article)
-        redirect_to @article
+        if @article.save
+            flash[:notice] = "Article was created successfully"
+            # redirect_to article_path(@article)
+            redirect_to @article
+        else
+            render "new"
+            # redirect_to new_article_path # no funciona porque al redireccionar se pierden los parametros
+        end
     end
 end
