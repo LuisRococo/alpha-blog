184ee4db7678119fdb4bea8e7fae5accb7013ea8
Luis Roberto <luisrobertopenav@gmail.com>
2022-08-09 15:16:48 -0700

    Add delete farticle feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -37,4 +37,10 @@ class ArticlesController < ApplicationController
     def edit ()
         @article = Article.find (params[:id])
     end
+
+    def destroy ()
+        @article = Article.find params[:id]
+        @article.destroy
+        redirect_to article_path
+    end
 end
