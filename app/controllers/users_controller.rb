399d51a413e46dd1a70fee6737f74756c7f0eb0f
Luis Roberto <luisrobertopenav@gmail.com>
2022-08-15 17:36:36 -0700

    Add cascade delete on users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
-    before_action :set_user, only: [:show, :edit, :update]
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
     before_action :require_user, only: [:edit, :update]
-    before_action :require_same_user, only: [:edit, :update]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def new
         @user = User.new
@@ -38,6 +38,15 @@ class UsersController < ApplicationController
     def index 
         @users = User.paginate(page: params[:page], per_page: 5)
     end
+
+    def destroy 
+        @user.destroy
+        session[:user_id] = nil
+        flash[:notice] = "User accout deleted"
+        redirect_to root_path
+    end
+
+
 
     private
 
